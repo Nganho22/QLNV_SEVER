eb33550b545c46f5d6445eefd9199eb26e88bc36
nganho <nganho2251@gmail.com>
2024-09-14 21:46:41 +0700

    ...

---
+++ QLNV_Point/src/main/java/QLNVPointAPI/controller/ProfileController.java
@@ -0,0 +1,35 @@
+package QLNVPointAPI.controller;
+
+import QLNVPointAPI.model.Felicitation;
+import QLNVPointAPI.model.*;
+import QLNVPointAPI.repository.*;
+
+import org.apache.hc.core5.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+@RestController
+@RequestMapping("/apiPoint")
+public class ProfileController {
+
+    @Autowired
+    private PointRepository repo;
+
+
+    @GetMapping("/")
+    public List<Felicitation> getAllFelicitation() {
+        return repo.findAll();
+    }
+}
---
+++ QLNV_Point/src/main/java/QLNVPointApplication.java
@@ -0,0 +1,17 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "QLNVPointAPI.repository")
+@EntityScan(basePackages = "QLNVPointAPI.model")
+@ComponentScan(basePackages = {"QLNVPointAPI", "QLNVPointAPI.controller", "QLNVPointAPI.service"})
+public class QLNVPointApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QLNVPointApplication.class, args);
+    }
+
+}
